c840da4007db1b5189a432a1d9594dede33d1cfb
EllySensei <diamantegabriel468@gmail.com>
2021-01-01 09:52:30 +0800

    fixing dmenu stuff

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "notosans:size=11",  "fontawesome:size=11"};
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "mononoki:size=11",  "fontawesome:size=11"};
+static const char dmenufont[]       = "mononoki:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
