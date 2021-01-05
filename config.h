1554dd33a917c46508cc90627c15ddbf181d0e25
EllySensei <diamantegabriel468@gmail.com>
2021-01-05 21:20:28 +0800

    nerd font compensation

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki:size=11",  "fontawesome:size=11"};
+static const char *fonts[]          = { "mononoki:size=11", /* "fontawesome:size=11" */ "meslolgs nf:size=11" };
 static const char dmenufont[]       = "mononoki:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
