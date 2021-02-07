d98f1b8f6fc30d517ca80d7d947799da3560c8ca
EllySensei <diamantegabriel468@gmail.com>
2021-02-07 11:07:23 +0800

    used the autostart from anybar instead

--- config.def.h
+++ config.def.h
@@ -9,9 +9,9 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
 static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
-static const char *fonts[]          = { "mononoki:size=11", /* "fontawesome:size=11" */ "meslolgs nf:size=11" };
-static const char dmenufont[]       = "mononoki:size=11";
+static const char *altbarcmd        = "$HOME/.launch-polybar.sh"; /* Alternate bar launch command */
+static const char *fonts[]          = { "mononoki:size=13", /* "fontawesome:size=11" */ "meslolgs nf:size=13" };
+static const char dmenufont[]       = "mononoki:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
